89aee99851d5e8ec4528e679d01770fdd3824c28
teelge <120889540+teelge@users.noreply.github.com>
2023-01-22 03:34:44 -0600

    Update SocksByT.sh

--- SocksByT.sh
+++ SocksByT.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 echo "Updating system packages..."
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get full-upgrade -y
+sudo apt-get autoremove -y
+sudo apt-get autoclean -y
 sudo apt install -y snapd
 sudo snap install shadowsocks-libev
 sudo mkdir -p /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev
@@ -19,7 +24,6 @@ echo "{
    \"timeout\":60,
    \"method\":\"chacha20-ietf-poly1305\",
    \"nameserver\":\"1.1.1.1\"
-
 }" | sudo tee /var/snap/shadowsocks-libev/common/etc/shadowsocks-libev/config.json
 
 sudo touch /etc/systemd/system/shadowsocks-libev-server@.service
@@ -43,3 +47,10 @@ sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
 echo "Shadowsocks-Libev server has been configured and started."
 
+# add the cron job
+echo "@daily  sudo apt update && sudo apt upgrade -y" | crontab -
+
+echo "Shadowsocks-Libev server has been configured and started successfully."
+echo "Server password: $password"
+echo "Server port: $port"
+echo "Password method: chacha20-ietf-poly1305"
